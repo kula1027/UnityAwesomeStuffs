c4b0fd54d763ec51717258f6976f8d7620c98261
kulakim <kula1027@gmail.com>
2021-04-06 23:18:23 +0900

    added UnityTransformExtension

---
+++ AwesomeSnippets/Assets/AwesomeSnippets/Samples/UnityExtensions/UnityExtenstionsSample.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace AwesomeSnippets {
+    public class UnityExtenstionsSample : MonoBehaviour {
+
+        private void Start() {
+            Debug.Log($"FindDescendant(\"Lvl4\"): {transform.FindDescendant("Lvl4").name}");
+
+            Debug.Log($"FindDescendantsAll(\"Lvl3\"): {transform.FindDescendantsAll("Lvl3").Count}");
+
+            Debug.Log($"GetDescendantAll: {transform.GetDescendantAll().Count}");
+        }
+    }
+}
---
+++ AwesomeSnippets/Assets/AwesomeSnippets/Scripts/UnityTransformExtension.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class UnityTransformExtension {
+
+    /// <summary>
+    /// find descendant by name matches first
+    /// </summary>
+    /// <param name="tr"></param>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static Transform FindDescendant(this Transform tr, string name) {
+        Queue<Transform> queue = new Queue<Transform>();
+
+        queue.Enqueue(tr);
+
+        while (queue.Count > 0) {
+            Transform current = queue.Dequeue();
+            if (current.name == name) {
+                return current;
+            }
+
+            foreach (Transform t in current) {
+                queue.Enqueue(t);
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// finds all of descendants by name
+    /// </summary>
+    /// <param name="tr"></param>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static List<Transform> FindDescendantsAll(this Transform transform, string name) {
+        Queue<Transform> queue = new Queue<Transform>();
+        List<Transform> retList = new List<Transform>();
+
+        queue.Enqueue(transform);
+
+        while (queue.Count > 0) {
+            Transform current = queue.Dequeue();
+
+            if (current.name == name) {
+                retList.Add(current);
+            }
+
+            foreach (Transform t in current) {
+                queue.Enqueue(t);
+            }
+        }
+
+        return retList;
+    }
+
+    /// <summary>
+    /// returns all of descendants including itself
+    /// </summary>
+    /// <param name="transform"></param>
+    /// <returns></returns>
+    public static List<Transform> GetDescendantAll(this Transform transform) {
+        Queue<Transform> queue = new Queue<Transform>();
+        List<Transform> retList = new List<Transform>();
+
+        queue.Enqueue(transform);
+
+        while (queue.Count > 0) {
+            Transform current = queue.Dequeue();
+
+            retList.Add(current);
+
+            foreach (Transform t in current.transform) {
+                queue.Enqueue(t);
+            }
+        }
+
+        return retList;
+    }
+}
