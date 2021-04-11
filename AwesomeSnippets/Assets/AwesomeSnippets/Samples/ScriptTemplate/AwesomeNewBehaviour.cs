489a6399f0de0a9b82cca587415b9e5fb797dc73
kulakim <kula1027@gmail.com>
2021-04-11 22:31:53 +0900

    added ScriptTemplate

---
+++ AwesomeSnippets/Assets/AwesomeSnippets/Samples/ScriptTemplate/AwesomeNewBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AwesomeNamespace {
+
+    public class AwesomeNewBehaviour : MonoBehaviour {
+
+        #region Mono Methods
+
+        private void Start() {
+        }
+
+        private void Awake() {
+        }
+
+        #endregion Mono Methods           
+    }
+}
