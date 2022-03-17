8e4e4f1bea3bbb00f0ca495b14da946002a9931a
sjkim <sungjin.kim@formalworks.com>
2022-03-17 10:54:50 +0900

    Double Series
    (cherry picked from commit ec9b9e88644b4ada08cafd3cb733f0917b5e7c48)

---
+++ AwesomeSnippets/Assets/AwesomeSnippets/Scripts/DoubleRect.cs
@@ -0,0 +1,78 @@
+﻿public struct DoubleRect {
+    private DoubleVec2 size;
+    private DoubleVec2 max;
+    private DoubleVec2 min;
+    private DoubleVec2 center;
+
+    public DoubleRect(DoubleVec2 center, DoubleVec2 size) {
+        this.center = center;
+        this.size = size;
+
+        this.max = new DoubleVec2(center.x + size.x * 0.5d, center.y + size.y * 0.5d);
+        this.min = new DoubleVec2(center.x - size.x * 0.5d, center.y - size.y * 0.5d);
+    }
+
+    public DoubleRect(double minX, double maxX, double minY, double maxY) {
+        this.max = new DoubleVec2(maxX, maxY);
+        this.min = new DoubleVec2(minX, minY);
+
+        this.center = new DoubleVec2((maxX + minX) * 0.5d, (maxY + minY) * 0.5d);
+        this.size = new DoubleVec2(maxX - minX, maxY - minY);
+    }
+
+    public DoubleVec2 Size {
+        get => size;
+        set {
+            size = value;
+
+            UpdateMinMax();
+        }
+    }
+
+    public DoubleVec2 Max {
+        get => max;
+        set {
+            max = value;
+
+            UpdateSizeCenter();
+        }
+    }
+
+    public DoubleVec2 Min {
+        get => min;
+        set {
+            min = value;
+
+            UpdateSizeCenter();
+        }
+    }
+
+    public DoubleVec2 Center {
+        get => center;
+        set {
+            center = value;
+
+            UpdateMinMax();
+        }
+    }
+
+    /// <summary>
+    /// x => height 1당 width의 비율
+    /// y => width 1당 height의 비율
+    /// </summary>
+    public DoubleVec2 Ratio {
+        get {
+            return new DoubleVec2(size.x / size.y, size.y / size.x);
+        }
+    }
+
+    private void UpdateSizeCenter() {
+        size = new DoubleVec2(max.x - min.x, max.y - min.y);
+        center = (max + min) * 0.5d;
+    }
+
+    private void UpdateMinMax() {
+        max = new DoubleVec2(center.x + size.x * 0.5d, center.y + size.y * 0.5d);
+        min = new DoubleVec2(center.x - size.x * 0.5d, center.y - size.y * 0.5d);
+    }
+}
---
+++ AwesomeSnippets/Assets/AwesomeSnippets/Scripts/DoubleVec2.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public struct DoubleVec2 {
+    public double x;
+    public double y;
+
+    public DoubleVec2(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static DoubleVec2 operator +(DoubleVec2 a) => a;
+
+    public static DoubleVec2 operator -(DoubleVec2 a) => new DoubleVec2(-a.x, a.y);
+
+    public static DoubleVec2 operator +(DoubleVec2 a, DoubleVec2 b)
+    => new DoubleVec2(a.x + b.x, a.y + b.y);
+
+    public static DoubleVec2 operator -(DoubleVec2 a, DoubleVec2 b)
+    => a + (-b);
+
+    public static DoubleVec2 operator *(DoubleVec2 a, double b)
+    => new DoubleVec2(a.x * b, a.y * b);
+
+    public static DoubleVec2 operator /(DoubleVec2 a, double b)
+    => new DoubleVec2(a.x / b, a.y / b);
+
+    public Vector2 ToUnityVec() => new Vector2((float)x, (float)y);
+
+    public override string ToString() {
+        return $"({x:F2}, {y:F2})";
+    }
+}
